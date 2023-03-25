92541d95d64aae10db373035063abe0bc1c81c5b
Preqsy <obbyprecious24@gmail.com>
2023-03-25 16:40:24 +0100

    initial commit

---
+++ check.js
@@ -0,0 +1,6 @@
+console.log("Hi");
+setTimeout(async function () {
+    console.log('next');
+}, 2000)
+
+console.log("Hello");
---
+++ model/book_model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const db = require('../config/db')
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        default: "----"
+    },
+    isbn: {
+        type: String
+    },
+    author: {
+        type: String,
+        default: '----'
+    }
+});
+
+const bookmodel = db.model('books', bookSchema);
+module.exports = bookmodel;
